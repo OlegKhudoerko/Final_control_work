59e18a791866adada59c93c63a54f09ea8ca4c15
OlegKhudoerko <110164546+OlegKhudoerko@users.noreply.github.com>
2023-05-30 13:58:07 +0500

    13 задание(добавлен класс)

---
+++ src/Encapsulation/PetOriginator.java
@@ -0,0 +1,14 @@
+﻿package Encapsulation;
+
+public class PetOriginator extends Originator {
+
+    @Override
+    protected Pet createNewPet(PetType type) {
+
+        return switch (type) {
+            case Dog -> new Dog();
+            case Cat -> new Cat();
+            case Hamster -> new Hamster();
+        };
+    }
+}
